a69e132bbb599409d849a86f53dcba4df1d77ea1
github suzunaan <hiroekota114@gmail.com>
2020-11-30 00:15:16 +0900

    メモ追加機能の追加

--- reimanageapp/urls.py
+++ reimanageapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TopView,MemoView,deletememo
+from .views import TopView,MemoView,deletememo,MemoCreate
 
 urlpatterns=[
     path('', TopView.as_view(), name="top"),
     path('memolist', MemoView.as_view(), name="memolist"),
     path('deletememo/<int:pk>', deletememo, name="deletememo"),
+    path('memocreate', MemoCreate.as_view(), name="memocreate")
 ]
--- reimanageapp/views.py
+++ reimanageapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from django.shortcuts import redirect
-from django.views.generic import TemplateView,ListView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView,ListView,CreateView
 from .models import MemoModel
+
 
 class TopView(TemplateView):
     template_name = "top.html"
@@ -11,8 +13,16 @@ class MemoView(ListView):
     template_name = "memolist.html"
     model = MemoModel
 
+class MemoCreate(CreateView):
+    template_name="memocreate.html"
+    model=MemoModel
+    fields=('goods',"goods_number")
+    success_url=reverse_lazy("memolist")
+
+
 @require_POST #削除ボタンが押された時＝POSTメゾットの時のみ削除する
 def deletememo(request,pk):
     post = MemoModel.objects.get(pk=pk)
     post.delete()
     return redirect('memolist')
+
